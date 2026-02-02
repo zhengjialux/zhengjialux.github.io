4cc34e5e8db7be22568cf4ab2c7f012d610d25d3
Will0 <15879398507@163.com>
2026-02-02 19:58:27 +0800

    Update config.js

--- Entry/bookmark/config.js
+++ Entry/bookmark/config.js
@@ -1478,6 +1478,12 @@ const config = {
       url: "https://datahub.com/",
       category: "工具",
       icon: "./icons/dataHub-favicon.webp",
+    },
+    {
+      title: "NPMX",
+      url: "https://npmx.dev/",
+      category: "社区",
+      icon: "https://npmx.dev/favicon.svg",
     }
   ],
   categoryIcons: {
@@ -1510,6 +1516,7 @@ const config = {
     Mark: "⭐",
   },
 };
+
 
 
 
